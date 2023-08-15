68fef458f1a217f3b70c7725cdcfe146aceeaf3b
fayezf <mohammadfayezahmed835@gmail.com>
2023-08-15 12:14:39 +0600

    firebase added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA-Pi3vPZyhIrLvvWiUMTVok_iEojb0ydM",
-    authDomain: "job-portal-e42d6.firebaseapp.com",
-    projectId: "job-portal-e42d6",
-    storageBucket: "job-portal-e42d6.appspot.com",
-    messagingSenderId: "474939547188",
-    appId: "1:474939547188:web:12d31601260d10818f3461"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
